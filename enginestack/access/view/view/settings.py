ed90073bb0a8db2ddeed15c56f4389709f79445e
Felix Pan <tinylambda@gmail.com>
2021-09-04 01:24:56 +0800

    enable django channels

--- enginestack/access/view/view/settings.py
+++ enginestack/access/view/view/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'view.wsgi.application'
+
+ASGI_APPLICATION = 'view.asgi.application'
 
 
 # Database
